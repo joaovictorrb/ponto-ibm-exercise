e742f9cc2236f8e88b63a637b10886c18f78beb3
joaovictorrb <joaov.rbrum@gmail.com>
2022-04-11 14:53:37 -0300

    Adding post method for points

--- server/app/Controllers/Http/PointController.js
+++ server/app/Controllers/Http/PointController.js
@@ -1,6 +1,26 @@
 'use strict'
 
+const PointModel = use('App/Models/Point')
+const Response = require('@adonisjs/session/src/VowBindings/Response')
+const moment = require('moment')
+
 class PointController {
+	async newPoint({ params, request, response }) {
+		let { dateHourUnix } = request.only("dateHour")
+		dateHourUnix = dateHourUnix*1000
+
+		let date = moment(dateHourUnix).utc().format('YYYY-MM-DD')
+ 
+		const storePoint = await PointModel.create({
+			user_id: params.userId,
+			date,
+			unixTimeStamp: dateHourUnix
+		})
+
+		if(!storePoint) return "Erro"
+
+		return storePoint
+	}
 }
 
 module.exports = PointController
--- server/start/routes.js
+++ server/start/routes.js
@@ -16,4 +16,5 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => 'Hello world')
+Route.post('/:userId/newPointRegistry/', 'Pointcontroller.newPoint')
+
