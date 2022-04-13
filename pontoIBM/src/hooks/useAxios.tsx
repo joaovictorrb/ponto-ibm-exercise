991b43e817f78ca8deaa7d2c0aa1eb6b2c11991d
joaovictorrb <joaov.rbrum@gmail.com>
2022-04-13 14:57:55 -0300

    TSCONFIG updated and hook axios was made

---
+++ pontoIBM/src/hooks/useAxios.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+axios.defaults.baseURL = 'http://127.0.0.1:3333/'
+
+const useAxios = (axiosParams: AxiosRequestConfig) => {
+    const [response, setResponse] = useState<AxiosResponse>()
+    const [error, setError] = useState<AxiosError>()
+    const [loading, setLoading] = useState(true)
+
+    const request = async (params: AxiosRequestConfig) => {
+        try {
+            const result = await axios.request(params)
+            setResponse(result)
+        } catch (err) {
+            setError(err)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        request(axiosParams);
+      },[]);
+
+    return { response, error, loading };
+}
