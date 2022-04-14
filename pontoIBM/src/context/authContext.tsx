71197452f4253738f7e7de7f572e746905752664
kaioesa <xkaio@hotmail.com>
2022-04-14 12:35:50 -0300

    Adicionado integraçao front e back (com erros)

--- pontoIBM/src/context/authContext.tsx
+++ pontoIBM/src/context/authContext.tsx
@@ -1,35 +1,121 @@
-import React, {createContext, useState} from 'react';
-import {signIn} from '../mockup/login';
+import React, {createContext, useCallback, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import useAxios from '../hooks/useAxios';
 
-interface AuthContextData {
-  signed: boolean;
-  user: object | null;
-  LogIn(): Promise<unknown>;
-  LogOut(): void;
-}
+import {signIn} from '../mockup/login';
+import {AuthContextData, UserType} from '../@types/user';
+import {Alert} from 'react-native';
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({children}) => {
-  const [user, setUser] = useState<object | null>(null);
+  const [userIsLogged, setUserIsLogged] = useState<object | null>(null);
 
+  const [loggedUser, setLoggedUser] = useState<UserType>({});
+  const [loading, setLoading] = useState<Boolean>();
+  const [error, setError] = useState('');
+
+  // Codigo do Kaio. Funcional.
   async function LogIn() {
     const response = await signIn();
 
-    setUser(response.user);
+    setUserIsLogged(response.user);
   }
 
   function LogOut() {
     const response = signIn();
 
-    setUser(null);
+    setUserIsLogged(null);
+  }
+  // -----------------------------
+
+  // Definir se está logado ou nao.
+  async function getUserSession(token: String) {
+    const res = useAxios({
+      method: 'POST',
+      url: 'login',
+    });
+
+    setUserIsLogged(res.response?.data); // define o login para a route.
+  }
+
+  // login. De onde vem o Token
+  async function userLogin(email: String, password: String) {
+    try {
+      const getUser = useAxios({
+        method: 'POST',
+        url: 'login',
+        data: {
+          email,
+          password,
+        },
+      });
+
+      console.log(getUser);
+
+      if (getUser.response?.status !== 201) {
+        Alert.alert(getUser.error?.message as string);
+        setError(getUser.error?.message as string);
+      }
+
+      // não sei direito quando usar a INTERFACE do usuario
+      const token = getUser.response!.data
+
+      await AsyncStorage.setItem(token);
+
+      await getUserSession(token);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(true);
+    }
   }
 
   return (
-    <AuthContext.Provider value={{signed: !!user, user, LogIn, LogOut}}>
+    <AuthContext.Provider
+      value={{signed: !!userIsLogged, userIsLogged, LogIn, LogOut, userLogin}}>
       {children}
     </AuthContext.Provider>
   );
 };
 
 export default AuthContext;
+
+// const userLogout = useCallback(
+//   async function () {
+//     setLoggedUser(null);
+//     // ?
+//     setError(undefined);
+//     setLoading(false);
+//     setLogin(false);
+//     await AsyncStorage.removeItem('Token');
+
+//     // Navegar para tela de login
+//   },[]
+//   // [navigate],
+// );
+
+// Verificaçao do token para o login. AutoLogin.
+
+// useEffect(() => {
+//   async function autoLogin() {
+//     // Veficar a questao do token
+//     const token = await AsyncStorage.getItem(@Ponto:token);
+
+//     // ?
+//     if (token) {
+//       try {
+//         setError(undefined);
+//         setLoading(true);
+
+//         await getUserSession();
+//       } catch (err) {
+//       } finally {
+//         setLoading(false);
+//       }
+//     } else {
+//       setLogin(false);
+//     }
+//   }
+//   autoLogin();
+// }, [userLogout, getUserSession]);
--- pontoIBM/src/hooks/useAxios.tsx
+++ pontoIBM/src/hooks/useAxios.tsx
@@ -1,28 +1,33 @@
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react';
 
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, {AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios';
 
-axios.defaults.baseURL = 'http://127.0.0.1:3333/'
+axios.defaults.baseURL = 'http://127.0.0.1:3333/';
 
 const useAxios = (axiosParams: AxiosRequestConfig) => {
-    const [response, setResponse] = useState<AxiosResponse>()
-    const [error, setError] = useState<AxiosError>()
-    const [loading, setLoading] = useState(true)
+  const [response, setResponse] = useState<AxiosResponse>();
+  const [error, setError] = useState<AxiosError>();
+  const [loading, setLoading] = useState(true);
 
-    const request = async (params: AxiosRequestConfig) => {
-        try {
-            const result = await axios.request(params)
-            setResponse(result)
-        } catch (err) {
-            setError(err)
-        } finally {
-            setLoading(false)
-        }
+  const request = async (params: AxiosRequestConfig) => {
+    try {
+      console.log('dentro do try axios')
+      const result = await axios.request(params);
+      console.log(result)
+      setResponse(result);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        request(axiosParams);
-      },[]);
+  useEffect(() => {
+    request(axiosParams);
+  }, [request]);
+  
 
-    return { response, error, loading };
-}
+  return {response, error, loading};
+};
+
+export default useAxios;
